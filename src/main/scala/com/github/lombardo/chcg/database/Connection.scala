049f8ef9d8fff65df65e9471c28c7c58cf097f9a
lombardo-chcg <dpa.lombardo@gmail.com>
2018-06-11 22:20:00 -0500

    add basic slick db wiring

---
+++ src/main/scala/com/github/lombardo/chcg/database/Connection.scala
@@ -0,0 +1,23 @@
+package com.github.lombardo.chcg.database
+
+import java.util.concurrent.TimeUnit.SECONDS
+
+import scala.concurrent.duration.Duration
+import slick.jdbc.JdbcBackend.Database
+
+object Connection {
+  private val POSTGRES_HOST = sys.env.getOrElse("POSTGRES_HOST", "localhost")
+  private val POSTGRES_PORT = sys.env.getOrElse("POSTGRES_PORT", "5432")
+  private val POSTGRES_PASSWORD = sys.env.getOrElse("POSTGRES_PASSWORD", "postgres")
+  private val POSTGRES_USER = sys.env.getOrElse("POSTGRES_PASSWORD", "postgres")
+
+  private val pgUrl = s"jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/exercises"
+  private val pgDriver = "org.postgresql.Driver"
+
+  val db = Database.forURL(
+    url = pgUrl,
+    user = POSTGRES_USER,
+    password = POSTGRES_PASSWORD,
+    driver = pgDriver
+  )
+}
