138e18a27ddc92ceeef7a8e80e7fa7dfaae63b1a
lombardo-chcg <dpa.lombardo@gmail.com>
2018-06-12 00:00:50 -0500

    remove basic implementations

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 The test suite requires a connection to a Postgres instance containing the full dataset from [https://pgexercises.com/](https://pgexercises.com/)
 
-A Dockerized PG image with the dataset has been created for convenience.  To use, simply run this command:
+A Dockerized PG image with the dataset has been created for convenience.  To use, run this command:
 
 ```sh
 docker run -d -p 5432:5432  \
@@ -15,9 +15,10 @@ docker run -d -p 5432:5432  \
   lombardo/postgresql-exercises-docker:0.0.1
 ```
 
-The default PG host is `localhost` and the default port is `5432`.  These values can be overridden via env vars (`POSTGRES_HOST`, `POSTGRES_PORT`) or hardcoded into the project by modifying this file: `src/main/scala/com/github/lombardo/chcg/database/Connection.scala` 
+By default, the test suite expects the PG host to be `localhost` and the port `5432`.  These values can be overridden via env vars (`POSTGRES_HOST`, `POSTGRES_PORT`) or hardcoded into the project by modifying the db connection object: 
+`src/main/scala/com/github/lombardo/chcg/database/Connection.scala` 
 
-More details: [https://github.com/lombardo-chcg/postgresql-exercises-docker](https://github.com/lombardo-chcg/postgresql-exercises-docker)
+Docker image source code: [https://github.com/lombardo-chcg/postgresql-exercises-docker](https://github.com/lombardo-chcg/postgresql-exercises-docker)
 
 ### Run Tests
 ```
--- src/main/scala/com/github/lombardo/chcg/exercises/Basic.scala
+++ src/main/scala/com/github/lombardo/chcg/exercises/Basic.scala
@@ -8,83 +8,59 @@ import scala.concurrent.Await
 
 object Basic extends PGExercise {
   def `Retrieve everything from a table`: Seq[FacilitiesRow] = {
-    val query = facilities.result
-    Await.result(db.run(query), defaultDuration)
+    /*
+      This first example shows the pattern to use for each problem:
+        - compose a query using the Slick DSL.  This example is trivial but the Slick docs will guide you
+        - create a DBIOAction by calling `.result` on the query
+        - run the query using the provided DatabaseDef: `Connection.db`
+        - Await the result and return it
+    */
+    val query = facilities
+    val dbAction = query.result
+    Await.result(db.run(dbAction), defaultDuration)
   }
 
   def `Retrieve specific columns from a table`: Seq[(String, BigDecimal)] = {
-    val query = facilities.map(f => (f.name, f.membercost)).result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `Control which rows are retrieved`: Seq[FacilitiesRow] = {
-    val query = facilities.filter(_.membercost > BigDecimal(0.00)).result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `Control which rows are retrieved - part 2`: Seq[(Int, String, BigDecimal, BigDecimal)] = {
-    val query = facilities
-      .filter(f => f.membercost > BigDecimal(0.00) && f.membercost < f.monthlymaintenance/BigDecimal(50.0))
-      .map(f => (f.facid, f.name, f.membercost, f.monthlymaintenance))
-      .result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `Basic string searches`: Seq[FacilitiesRow] = {
-    val query = facilities.filter(_.name like "%Tennis%").result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `Matching against multiple possible values`: Seq[FacilitiesRow] = {
-    val targetValues = Set(1, 5)
-    val query = facilities.filter(_.facid inSet targetValues).result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `Classify results into buckets`: Seq[(String, String)] = {
-    val query = facilities.map(f =>
-      (
-        f.name,
-        Case
-          If(f.monthlymaintenance > BigDecimal(100)) Then "expensive"
-          Else "cheap"
-      )
-    ).result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `Working with dates`: Seq[(Int, String, String, java.sql.Timestamp)] = {
-    val query = members
-      .filter(_.joindate > java.sql.Timestamp.valueOf("2012-09-01 00:00:00"))
-      .map(m => (m.memid, m.surname, m.firstname, m.joindate))
-      .result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `Removing duplicates, and ordering results`: Seq[String] = {
-    val query = members.map(_.surname).distinct.sorted.take(10).result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `Combining results from multiple queries`: Seq[String] = {
-    val query = (members.map(_.surname) union facilities.map(_.name)).result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `Simple aggregation`: Option[java.sql.Timestamp] = {
-    val query = members.map(_.joindate).max.result
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 
   def `More aggregation`: Seq[(String, String, java.sql.Timestamp)] = {
-    val mostRecentMembersQuery = for {
-      member <- members
-      mostRecentJoinDate <- Query(members.map(_.joindate).max)
-      if member.joindate === mostRecentJoinDate
-    } yield (member.firstname, member.surname, member.joindate)
-
-    val query = mostRecentMembersQuery.result
-
-    Await.result(db.run(query), defaultDuration)
+    ???
   }
 }
